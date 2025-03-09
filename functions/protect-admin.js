553c68d4c71adab0a680c9f713f70c201cbeb5a4
Lex Eagle <lexeagleindia@gmail.com>
2025-03-10 00:36:38 +0530

    Update protect-admin.js

--- functions/protect-admin.js
+++ functions/protect-admin.js
@@ -1,9 +1,11 @@
-// functions/protect-admin.js
-const validToken = process.env.ADMIN_ACCESS_TOKEN; 
+=
+const validToken = process.env.ADMIN_ACCESS_TOKEN;
 
 exports.handler = async (event, context) => {
+
   const { queryStringParameters } = event;
   const token = queryStringParameters.token;
+
 
   if (token === validToken) {
 
@@ -14,8 +16,11 @@ exports.handler = async (event, context) => {
   } else {
 
     return {
-      statusCode: 403,
-      body: JSON.stringify({ message: "Access denied!" }),
+      statusCode: 302,  
+      headers: {
+        Location: '/',
+      },
+      body: JSON.stringify({ message: "Access denied. Redirecting to login." }),
     };
   }
 };
